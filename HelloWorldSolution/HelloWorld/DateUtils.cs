2a76537cebd34c6fbc83563ea327b2a998c766f6
Steven Eler <Steven_Eler@Progressive.com>
2022-12-05 15:20:27 -0500

    console break timer

---
+++ HelloWorldSolution/HelloWorld/DateUtils.cs
@@ -0,0 +1,10 @@
+﻿
+namespace HelloWorld;
+
+public class DateUtils
+{
+    public DateTime TakeABreak(int minutes)
+    {
+        return DateTime.Now.AddMinutes(minutes);
+    }
+}
---
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using HelloWorld;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Take a break!");
+
+Console.Write("How many minutes?: ");
+string? minutes = Console.ReadLine();
+
+if (minutes is not null)
+{
+    DateUtils utils = new DateUtils();
+
+    int mins = int.Parse(minutes);
+    DateTime timeAtEndOfBreak = utils.TakeABreak(mins);
+    Console.WriteLine($"Ok, be back at {timeAtEndOfBreak:T}");
+}
+else
+{
+    Console.WriteLine("Enter some numbers, dude");
+}
