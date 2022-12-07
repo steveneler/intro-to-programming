3a0fe732b4dd0698648bf46df7108fae2f0bef4b
Steven Eler <Steven_Eler@Progressive.com>
2022-12-07 11:09:24 -0500

    done with inheritance

--- BankingSolution/Banking.Domain/BankAccount.cs
+++ BankingSolution/Banking.Domain/BankAccount.cs
@@ -8,7 +8,7 @@ public class BankAccount
 
     private decimal _balance = 5000;
 
-    public void Deposit(decimal amountToDeposit)
+    public virtual void Deposit(decimal amountToDeposit)
     {
         _balance += amountToDeposit;
     }
